2b7bab729e164c73d628b9deb37e15e01aa95097
vaibhav7000 <vchawla7000@gmail.com>
2025-06-07 11:08:32 +0530

    simple sum backend server

--- index.js
+++ index.js
@@ -1,15 +1,69 @@
-const express = require('express');
-const { resolve } = require('path');
-
+const express = require("express");
+const z = require("zod");
 const app = express();
-const port = 3010;
+const port = 3000;
 
-app.use(express.static('static'));
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
-});
+// sending the numbers in query-params
+app.get("/findsum",function(req, res) {
+  let firstNumber = req.query.a;
+  let secondNumber = req.query.b;
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+  // want to be valid numbers in the form of string
+  const numberSchema = z.coerce.number(); // will try to convert the given input into number
+
+  let result = numberSchema.safeParse(firstNumber);
+
+  if(!result.success) {
+    res.status(411).json({
+      msg: "Invalid firstNumber",
+      issues: result.error.issues
+    })
+    return
+  }
+
+  firstNumber = result.data;
+
+  
+
+  result = numberSchema.safeParse(secondNumber);
+
+  if(!result.success) {
+    res.status(411).json({
+      msg: "Invalid second",
+      issues: result.error.issues
+    })
+    return
+  }
+
+  secondNumber = result.data;
+
+  res.status(200).send(`${firstNumber + secondNumber}`);
+
+})
+
+
+
+app.use(function(err, req, res, next) {
+  if(err) {
+    res.status(500).json({
+      msg: "Internal server error",
+    })
+    return
+  }
+
+  next();
+})
+
+
+app.use(function(req, res) {
+  res.status(404).json({
+    msg: "Route not found"
+  })
+})
+
+
+
+app.listen(port, function() {
+  console.log("server started");
+})
